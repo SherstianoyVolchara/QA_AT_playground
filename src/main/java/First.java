8902d0aad45afe9142fa2b4730d149102427c4a0
SherstianoyVolchara <alfiiaqa@gmail.com>
2022-02-17 21:51:33 +0200

    hw5

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,6 +1,7 @@
 public class First {
     public static void main(String[] args) {
         Second second = new Second();
-        System.out.println("~~~" + second.showNumber() + "~~~");
+        int temp = second.showNumber();
+        System.out.printf("~~~%d~~~", temp);
     }
 }
--- src/main/java/Second.java
+++ src/main/java/Second.java
@@ -1,7 +1,7 @@
 public class Second {
     public int showNumber() {
         int number = 574;
-        System.out.println("---" + number + "---");
+        System.out.printf("---%d---\n", number);
         return number;
     }
 }
