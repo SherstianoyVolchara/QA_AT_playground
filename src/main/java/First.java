159896a1d133812e5159b807965bd6b96ed9f15b
SherstianoyVolchara <alfiiaqa@gmail.com>
2022-02-17 20:44:39 +0200

    hw5

---
+++ src/main/java/First.java
@@ -0,0 +1,6 @@
+public class First {
+    public static void main(String[] args) {
+        Second second = new Second();
+        System.out.println("~~~" + second.showNumber() + "~~~");
+    }
+}
---
+++ src/main/java/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+    public int showNumber() {
+        int number = 574;
+        System.out.println("---" + number + "---");
+        return number;
+    }
+}
